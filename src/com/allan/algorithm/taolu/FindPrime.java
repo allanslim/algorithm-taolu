4afb27bb1c95ae5d40f9d945da34a2be7119f36a
Allan Lim <allanslim@gmail.com>
2018-12-31 11:58:42 -0800

    added efficient prime calculation

--- src/com/allan/algorithm/taolu/FindPrime.java
+++ src/com/allan/algorithm/taolu/FindPrime.java
@@ -9,16 +9,38 @@ public class FindPrime {
         boolean isPrime = isPrime(number);
         System.out.println(isPrime);
 
-        for(int i = 1; i < 200; i ++) {
-            if(isPrime(i)) {
+        for (int i = 1; i < 200; i++) {
+            if (isPrime(i)) {
+                System.out.println(i);
+            }
+        }
+
+        for (int i = 1; i < 200; i++) {
+            if (isPrimeEfficient(i)) {
                 System.out.println(i);
             }
         }
     }
 
     private static boolean isPrime(int number) {
-        for( int i = 2; i < number; i++) {
-            if(number % i == 0) {
+        if (number < 2) {
+            return false;
+        }
+        for (int i = 2; i < number; i++) {
+            if (number % i == 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    private static boolean isPrimeEfficient(int number) {
+        if (number < 2) {
+            return false;
+        }
+        int sqrt = (int) Math.sqrt(number);
+        for (int i = 2; i <= sqrt; i++) {
+            if (number % i == 0) {
                 return false;
             }
         }
